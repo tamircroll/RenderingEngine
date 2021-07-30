eb084b668e63b5bda976e2105c7c780abdf55a35
Tamir croll <tamircroll@gmail.com>
2021-07-30 15:22:58 +0300

    First commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+import scala.tools.nsc.Main
+
+object Main
+{
+    def main(args : Array[String]) : Unit =
+    {
+        println("Tamir here ! ! !")
+    }
+}
---
+++ src/main/scala/model/Composition.scala
@@ -0,0 +1,10 @@
+package model
+
+case class Resolution()
+
+case class Composition(Resolution : Resolution, layers : List[Layers])
+{
+
+}
+
+
