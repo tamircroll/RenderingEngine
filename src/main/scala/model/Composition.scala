5d10cbbbcd0211aba60442c0451302953b4fc5b8
Tamir croll <tamircroll@gmail.com>
2021-07-30 19:38:18 +0300

    Some progress..

--- src/main/scala/model/Composition.scala
+++ src/main/scala/model/Composition.scala
@@ -1,10 +1,17 @@
 package model
 
-case class Resolution()
+import java.io.File
+import scala.collection.immutable
 
-case class Composition(Resolution : Resolution, layers : List[Layers])
+case class Resolution(width : Int, height : Int)
+
+case class Composition(file : File, resolution : Resolution, layers : List[SRLayer]) extends CriticalTimeStampObserver
 {
-
+    val allCriticalTimeStamps : List[Long] = layers.flatMap(layer => (layer.getCriticalTimeStamps()))
+    
+    override def onCriticalTimeStamp(timeStamp : Long) : File
+    {
+    
+    }
 }
-
 
